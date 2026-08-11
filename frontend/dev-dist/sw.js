17c4d6a620ea7d90b419d8aa86323a26435f3a2f
Manish Jha <manishkrjha2005@gmail.com>
2026-08-11 21:18:37 +0530

    Add Railway deployment config - DATABASE_URL support + railway.json

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.6p4s6hro88o"
+    "revision": "0.63r5fb0i4c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
